3e69c81e8ca4980e0a1a27b67c07fcbf470d095a
Leslie Helou <leslie@ccs-indy.com>
2017-05-03 23:40:07 -0400

    update

--- jamfStatus/StatusMenuController.swift
+++ jamfStatus/StatusMenuController.swift
@@ -1,5 +1,6 @@
 //StatusMenuController.swift
-//Copyright 2017 Leslie N. Helou
+//Author: Leslie Helou
+//Copyright 2017 Jamf Professional Services
 //
 //Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 //
