7edad35422101d56c5a924ffd0c857e17fae8c40
Leslie Helou <leslie@ccs-indy.com>
2017-09-10 09:48:17 -0400

    code updates

--- jamfStatus/AppDelegate.swift
+++ jamfStatus/AppDelegate.swift
@@ -15,7 +15,8 @@ import WebKit
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate, URLSessionDelegate {
 
-    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var cloudStatus_Toolbar: NSToolbar!
+    @IBOutlet weak var cloudStatusWindow: NSWindow!
     @IBOutlet weak var page_WebView: WebView!
     @IBOutlet weak var prefs_Panel: NSPanel!
     @IBOutlet weak var pollingInterval_TextField: NSTextField!
@@ -43,6 +44,8 @@ class AppDelegate: NSObject, NSApplicationDelegate, URLSessionDelegate {
         about_WebView.loadFileURL(fileUrl as URL, allowingReadAccessTo: baseUrl as URL)
         
         NSApplication.shared().activate(ignoringOtherApps: true)
+
+        cloudStatusWindow.titleVisibility = NSWindowTitleVisibility.hidden
         about_NSWindow.setIsVisible(true)
     }
     
@@ -50,15 +53,13 @@ class AppDelegate: NSObject, NSApplicationDelegate, URLSessionDelegate {
     @IBAction func viewStatus(_ sender: Any) {
         page_WebView.mainFrameURL = SMC.statusURL
         NSApplication.shared().activate(ignoringOtherApps: true)
-        window.setIsVisible(true)
+        cloudStatusWindow.setIsVisible(true)
     }
     
     @IBOutlet weak var prefWindowAlerts_Button: NSButton!
     @IBOutlet weak var prefWindowIcon_Button: NSButton!
     
-    
     @IBAction func prefs_MenuItem(_ sender: NSMenuItem) {
-        
         let local_pollingInterval = SMC.readSettings()?["pollingInterval"]  as! Int32
         pollingInterval_TextField.stringValue = "\(local_pollingInterval)"
         
@@ -89,49 +90,6 @@ class AppDelegate: NSObject, NSApplicationDelegate, URLSessionDelegate {
         prefs_Panel.setIsVisible(true)
     }
     
-//    @IBAction func pollingInterval_Action(_ sender: Any) {
-//        pollingInterval = 300
-//        if let interval = UInt32(pollingInterval_TextField.stringValue) {
-//            if interval >= 120 {
-//                pollingInterval = interval
-//            }
-//        }
-//        SMC.settingsPlistData["pollingInterval"] = pollingInterval
-//        SMC.settingsPlistData["hideUntilStatusChange"] = (prefWindowAlerts_Button.state == 0 ? false:true)
-//        SMC.settingsPlistData["hideMenubarIcon"] = (prefWindowIcon_Button.state == 0 ? false:true)
-//        SMC.settingsPlistData["launchAgent"] = (launchAgent_Button.state == 0 ? false:true)
-//        // Write info to settings.plist
-//        (SMC.settingsPlistData as NSDictionary).write(toFile: SMC.SettingsPlistPath, atomically: false)
-//    }
-//    
-//    @IBAction func prefWindowAlert_Action(_ sender: NSButton) {
-//        SMC.settingsPlistData["pollingInterval"] = SMC.readSettings()?["pollingInterval"]  as! Int32
-//        SMC.settingsPlistData["hideUntilStatusChange"] = (prefWindowAlerts_Button.state == 0 ? false:true)
-//        SMC.settingsPlistData["hideMenubarIcon"] = (prefWindowIcon_Button.state == 0 ? false:true)
-//        SMC.settingsPlistData["launchAgent"] = (launchAgent_Button.state == 0 ? false:true)
-//        // Write info to settings.plist
-//        (SMC.settingsPlistData as NSDictionary).write(toFile: SMC.SettingsPlistPath, atomically: false)
-//    }
-//    
-//    @IBAction func prefWindowIcon_Action(_ sender: NSButton) {
-//        SMC.settingsPlistData["pollingInterval"] = SMC.readSettings()?["pollingInterval"]  as! Int32
-//        SMC.settingsPlistData["hideUntilStatusChange"] = (prefWindowAlerts_Button.state == 0 ? false:true)
-//        SMC.settingsPlistData["hideMenubarIcon"] = (prefWindowIcon_Button.state == 0 ? false:true)
-//        SMC.settingsPlistData["launchAgent"] = (launchAgent_Button.state == 0 ? false:true)
-//        // Write info to settings.plist
-//        (SMC.settingsPlistData as NSDictionary).write(toFile: SMC.SettingsPlistPath, atomically: false)
-//    }
-//    
-//    @IBAction func launchAgent_Action(_ sender: NSButton) {
-//        SMC.settingsPlistData["pollingInterval"] = SMC.readSettings()?["pollingInterval"]  as! Int32
-//        SMC.settingsPlistData["hideUntilStatusChange"] = (prefWindowAlerts_Button.state == 0 ? false:true)
-//        SMC.settingsPlistData["hideMenubarIcon"] = (prefWindowIcon_Button.state == 0 ? false:true)
-//        SMC.settingsPlistData["launchAgent"] = (launchAgent_Button.state == 0 ? false:true)
-//        // Write info to settings.plist
-//        (SMC.settingsPlistData as NSDictionary).write(toFile: SMC.SettingsPlistPath, atomically: false)
-//    }
-    
-    
     @IBAction func back_button(_ sender: Any) {
         page_WebView.goBack()
     }
@@ -156,7 +114,7 @@ class AppDelegate: NSObject, NSApplicationDelegate, URLSessionDelegate {
         var isDir: ObjCBool = true
         pollingInterval = 300
         if let interval = UInt32(pollingInterval_TextField.stringValue) {
-            if interval >= 120 {
+            if interval >= 60 {
                 pollingInterval = interval
             }
         }
--- jamfStatus/StatusMenuController.swift
+++ jamfStatus/StatusMenuController.swift
@@ -17,10 +17,14 @@ class StatusMenuController: NSObject, URLSessionDelegate {
 
     @IBOutlet weak var alert_window: NSPanel!
     @IBOutlet weak var cloudStatusMenu: NSMenu!
+    @IBOutlet weak var status_Toolbar: NSToolbar!
     
+    @IBOutlet weak var alert_TextView: NSTextField!
     @IBOutlet weak var alert_TextFieldCell: NSTextFieldCell!
-    @IBOutlet weak var alert_ImageCell: NSImageCell!
     
+//    @IBOutlet var alert_TextView: NSTextView!
+//    @IBOutlet weak var alert_TextFieldCell: NSTextFieldCell!
+    @IBOutlet weak var alert_ImageCell: NSImageCell!
     
     let fileManager = FileManager.default
     let cloudStatusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
@@ -30,10 +34,11 @@ class StatusMenuController: NSObject, URLSessionDelegate {
     var theResult = ""
     var iconName = ""
     var icon = NSImage(named: "cloudStatus-red")
-    let services = ["JSS Hosting - US", "JSS Hosting - EU", "Jamf Now", "Jamf Nation", "Jamf Software Website", "Jamf Cloud Push Proxy", "Jamf Cloud Distribution Service", "CDN Services", "DNS", "Compute Services - US", "Database Services - US", "Storage Services - US", "Storage Services - EU", "Jamf Cloud Provisioning Services", "Email Services"]
-    
     
     @IBOutlet weak var alertWindowPref_Button: NSButton!
+    var alert_header = ""
+    var alert_message = ""
+    var serviceCount = 0
     var alert_image_green = NSImage(named: "caution-green")
     var alert_image_yellow = NSImage(named: "caution-yellow")
     var alert_image_red = NSImage(named: "caution-red")
@@ -47,8 +52,7 @@ class StatusMenuController: NSObject, URLSessionDelegate {
         
     var settingsPlistData:[String:Any] = [:]
     
-    var statusLine = ""
-    var statusLine2 = ""
+    var affectedServices = ""
 
     @IBOutlet weak var alert_ImageView: NSImageView!
 
@@ -68,35 +72,24 @@ class StatusMenuController: NSObject, URLSessionDelegate {
             while true {
                 AppDlg.setDefaultPrefs()
                 AppDlg.pollingInterval = UInt32(self.readSettings()?["pollingInterval"]  as! Int32)
-                if AppDlg.pollingInterval < 120 {
+                if AppDlg.pollingInterval < 60 {
                     AppDlg.pollingInterval = 300
                     self.settingsPlistData["hideUntilStatusChange"] = self.readSettings()?["hideUntilStatusChange"]  as! Bool
                     self.settingsPlistData["pollingInterval"] = 300     //as Any?
                     // Write info to settings.plist
                     (self.settingsPlistData as NSDictionary).write(toFile: self.SettingsPlistPath, atomically: false)
                 }
-                AppDlg.hideIcon = Bool(self.readSettings()?["hideMenubarIcon"]  as! Bool)
-                //print("Poll Interval: \(AppDlg.pollingInterval)")
-//                print("hide icon: \(AppDlg.hideIcon)")
-                
-                self.iconName = self.getStatus2()
-                for _ in 0...1 {
-                    if !(AppDlg.hideIcon) {
-                        self.icon = NSImage.init(named: self.iconName)
-                    } else {
-                        self.icon = NSImage.init(named: "minimizedIcon")
+                print("checking status")
+                self.getStatus2() {
+                    (result: String) in
+                        
+                    DispatchQueue.main.async {
+                        self.iconName = result
+                        AppDlg.hideIcon ? (self.icon = NSImage.init(named: "minimizedIcon")):(self.icon = NSImage.init(named: self.iconName))
+
+                            self.cloudStatusItem.image = self.icon
                     }
-                    self.cloudStatusItem.image = self.icon
-                    // added additional sleep and icon mumbo jumbo to get icon to refresh
-                    sleep(5)
                 }
-//                if !(AppDlg.hideIcon) {
-//                    self.icon = NSImage.init(named: self.iconName)
-//                } else {
-//                    self.icon = NSImage.init(named: "minimizedIcon")
-//                }
-//                self.cloudStatusItem.image = self.icon
-                
                 sleep(UInt32(AppDlg.pollingInterval))
             }
         }
@@ -104,152 +97,154 @@ class StatusMenuController: NSObject, URLSessionDelegate {
     
     @IBAction func alertWindowPref_Action(_ sender: NSButton) {
         settingsPlistData["pollingInterval"] = readSettings()?["pollingInterval"]  as! Int32
+        settingsPlistData["hideMenubarIcon"] = readSettings()?["hideMenubarIcon"]  as! Bool
         if alertWindowPref_Button.state == 0 {
             settingsPlistData["hideUntilStatusChange"] = false
         } else {
             settingsPlistData["hideUntilStatusChange"] = true
         }
         // Write info to settings.plist
-        (settingsPlistData as NSDictionary).write(toFile: SettingsPlistPath, atomically: false)
+        (settingsPlistData as NSDictionary).write(toFile: SettingsPlistPath, atomically: true)
     }
     
     func displayAlert(currentState: String) {
-        // adjust font size so that alert message fits in text box.
-        if statusLine2.characters.count > 55 {
-            self.alert_TextFieldCell.font = NSFont(name: "System", size: 14.0)
-        } else {
-            self.alert_TextFieldCell.font = NSFont(name: "System", size: 18.0)
-        }
-        if (readSettings()?["hideUntilStatusChange"] as! Bool) {
-            alertWindowPref_Button.state = NSOnState
-        } else {
-            alertWindowPref_Button.state = NSOffState
-        }
-        if prevState != currentState {
-            DispatchQueue.main.async {
-                if self.alert_window.isVisible {
-                    self.alert_window.setIsVisible(false)
-                }
-                self.alert_window.setIsVisible(true)
+        var alertHeight = 0
+        DispatchQueue.main.async {
+            // adjust font size so that alert message fits in text box.
+            print("count: \(self.alert_message.characters.count)")
+            self.serviceCount > 2 ? (alertHeight = 99 + 18*(self.serviceCount-2)):(alertHeight = 99)
+            self.alert_window.setContentSize(NSSize(width: 398, height:alertHeight))
+            if self.alert_message.characters.count > 55 {
+                self.alert_TextView.font = NSFont(name: "Arial", size: 12.0)
+            } else {
+                self.alert_TextView.font = NSFont(name: "Arial", size: 18.0)
             }
-        } else {
-            if !((self.readSettings()?["hideUntilStatusChange"]) as! Bool) && prevState != "cloudStatus-green" {
+            if (self.readSettings()?["hideUntilStatusChange"] as! Bool) {
+                self.alertWindowPref_Button.state = NSOnState
+            } else {
+                self.alertWindowPref_Button.state = NSOffState
+            }
+            if self.prevState != currentState {
                 DispatchQueue.main.async {
-                    if self.alert_window.isVisible {
-                        self.alert_window.setIsVisible(false)
+                    self.refreshAlert()
+                }
+            } else {
+                if !((self.readSettings()?["hideUntilStatusChange"]) as! Bool) && self.prevState != "cloudStatus-green" {
+                    DispatchQueue.main.async {
+                        self.refreshAlert()
                     }
-                    self.alert_window.setIsVisible(true)
                 }
             }
-        }
-        prevState = currentState
+            self.prevState = currentState
+        }   // DispatchQueue.main.async - end
     }
     
-    func getStatus2() -> String {
-        var localResult = ""
-        var indexOfA:Int?
-        var statusIndex:Int?
-        var counter = 0
-
-        var service:[String] = []
-        
-        let myURLString = self.statusURL
-        guard let myURL = URL(string: myURLString) else {
-            print("Error: \(myURLString) doesn't seem to be a valid URL")
-            return("Error")
+    func refreshAlert() {
+        self.alert_window.title = "\(alert_header)"
+        self.alert_TextFieldCell.stringValue = self.alert_message
+        if self.alert_window.isVisible {
+            self.alert_window.setIsVisible(false)
+            sleep(1)
         }
-        
-        do {
-            let statusPageString = try String(contentsOf: myURL, encoding: .ascii)
-            //print("\(statusPageString)")
-            service = statusPageString.components(separatedBy: "\n")
-            // remove leading spaces and tabs
-            for theLine in service {
-                service[counter] = theLine.trimmingCharacters(in: .whitespaces).replacingOccurrences(of: "\t", with: "")
-                counter += 1
-            }
-            // try to capture status message from web page - start
-            let marker = service.index(of: "<span class=\"status font-large\">")
-            let marker2 = service.index(of: "<div class=\"incident-title font-large\">")
-//            print("marker: \(String(describing: marker))\n")
-//            print("marker2: \(String(describing: marker2))\n")
-            if marker != nil {
-                alert_TextFieldCell.stringValue = "Jamf Cloud Status:\n\(service[marker!+1])"
-                print("Jamf Cloud: \(service[marker!+1])")
-            } else if marker2 != nil {
-                statusLine = "\(service[marker2!+1])"
-                print("statusLine: \(service[marker2!+1])\n")
-                statusLine2 = ""
-                //statusLine2 = statusMsg(theLine: statusLine)
-                // substring
-                if let start = statusLine.range(of: ">"),
-                    let end  = statusLine.range(of: "</a>", range: start.upperBound..<statusLine.endIndex) {
-                    statusLine2.append(statusLine[start.upperBound..<end.lowerBound])
-                } else {
-                    print("invalid input")
-                }
-                alert_TextFieldCell.stringValue = "Jamf Cloud Status:\n\(statusLine2)"
-                print("Jamf Cloud: \(statusLine2)")
-            } else {
-                alert_TextFieldCell.stringValue = ""
-                print("status not found")
-            }
-            // try to capture status message from web page - end
-            
-            for theService in services {
-                indexOfA = service.index(of: theService)
-                if indexOfA != nil {
-                    statusIndex = indexOfA!+1
-                    while service[statusIndex!].trimmingCharacters(in: .whitespacesAndNewlines) != "<span class=\"component-status\">" {
-                        statusIndex = statusIndex! + 1
-                    }
-                    statusIndex = statusIndex! + 1
-                    print("\(theService)".trimmingCharacters(in: .whitespacesAndNewlines) + ": \(service[statusIndex!])")
-                }
-            }
-            if statusPageString.lowercased().contains("all systems operational") {
-                localResult = "cloudStatus-green"
-                alert_ImageView.image = alert_image_green
-                if alert_TextFieldCell.stringValue == "" {
-                    alert_TextFieldCell.stringValue = "Jamf Cloud status is fully operational."
-                }
-                displayAlert(currentState: localResult)
-            } else if statusPageString.lowercased().contains("page-status status-minor") || statusPageString.lowercased().contains("unresolved-incident impact-minor") || statusPageString.lowercased().contains("unresolved-incident impact-none") {
-                localResult = "cloudStatus-yellow"
-                alert_ImageView.image = alert_image_yellow
-                if alert_TextFieldCell.stringValue == "" {
-                    alert_TextFieldCell.stringValue = "Please be aware there is a minor issue that may affect your Jamf Cloud instance."
-                }
-                displayAlert(currentState: localResult)
-                
-            } else if statusPageString.lowercased().contains("page-status status-maintenance") && statusPageString.lowercased().contains("experiencing issues with email services") {
-                localResult = "cloudStatus-yellow"
-                alert_ImageView.image = alert_image_yellow
-                if alert_TextFieldCell.stringValue == "" {
-                    alert_TextFieldCell.stringValue = "Please be aware there is a minor issue that should not affect your Jamf Cloud instance."
-                }
-                displayAlert(currentState: localResult)
-                
-            } else {
-                localResult = "cloudStatus-red"
-                alert_ImageView.image = alert_image_red
-                if alert_TextFieldCell.stringValue == "" {
-                    alert_TextFieldCell.stringValue = "Please be aware there is a major issue that may affect your Jamf Cloud instance."
-                }
-                displayAlert(currentState: localResult)
-            }
-            
-            print("localResult2: \(localResult)\n")
-        } catch let error {
-            print("Error: \(error)")
-        }
-        
-        if (localResult != "cloudStatus-green") && (localResult != "cloudStatus-yellow") && (localResult != "cloudStatus-red") {
-            return("minimizedIcon")
-        } else {
-            return(localResult)
-        }
+        self.alert_window.setIsVisible(true)
     }
+    
+//    func getStatus2() -> String {
+    func getStatus2(completion: @escaping (_ result: String) -> Void) {
+        var localResult = ""
+        
+        var operationalArray = [String]()
+        var warningArray = [String]()
+        var criticalArray = [String]()
+        
+        operationalArray.removeAll()
+        warningArray.removeAll()
+        criticalArray.removeAll()
+        affectedServices = ""
+        URLCache.shared.removeAllCachedResponses()
+        
+        //        JSON parsing - start
+        let apiStatusUrl = "https://status.jamf.com/api/v2/components.json"
+//        let apiStatusUrl = "http://172.27.1.35/components.json"
+        let encodedURL = NSURL(string: apiStatusUrl)
+        let request = NSMutableURLRequest(url: encodedURL! as URL)
+        request.httpMethod = "GET"
+        let configuration = URLSessionConfiguration.default
+        
+        configuration.httpAdditionalHeaders = ["Accept" : "application/json"]
+        let session = Foundation.URLSession(configuration: configuration, delegate: self, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {
+            (data, response, error) -> Void in
+            if (response as? HTTPURLResponse) != nil {
+//                print("httpResponse: \(String(describing: response))")
+                do {
+                    if let data = data,
+                        let json = try JSONSerialization.jsonObject(with: data) as? [String: Any],
+                        let cloudServices = json["components"] as? [[String: Any]] {
+                        for cloudService in cloudServices {
+                            if let name = cloudService["name"] as? String,
+                                let status = cloudService["status"] as? String {
+                                switch status {
+                                case "degraded_performance", "partial_outage":
+                                    warningArray.append(name + ": " + status)
+                                case "major_outage":
+                                    criticalArray.append(name + ": " + status)
+                                default:
+                                    operationalArray.append(name + ": " + status)
+                                }
+                            }
+                        }
+                    }
+                } catch {
+                    print("Error deserializing JSON: \(error)")
+                }   // do - end
+            }   // if let httpResponse - end
+            if criticalArray.count > 0 {
+                self.alert_header = "Jamf Cloud Critical Issue Alert"
+                localResult = "cloudStatus-red"
+                for service in criticalArray {
+                    self.affectedServices.append("    \(service)\n")
+                }
+                self.alert_ImageView.image = self.alert_image_red
+                    self.alert_message = "Please be aware there is a major issue that may affect your Jamf Cloud instance.\n\(self.affectedServices)"
+                self.serviceCount = criticalArray.count
+                self.displayAlert(currentState: localResult)
+            } else if warningArray.count > 0 {
+                self.alert_header = "Jamf Cloud Minor Issue Alert"
+                localResult = "cloudStatus-yellow"
+                for service in warningArray {
+                    self.affectedServices.append("    \(service)\n")
+                }
+                self.alert_ImageView.image = self.alert_image_yellow
+                self.alert_message = "Please be aware there is a minor issue that may affect your Jamf Cloud instance.\n\(self.affectedServices)"
+                self.serviceCount = warningArray.count
+                self.displayAlert(currentState: localResult)
+            } else {
+                self.alert_header = "Notice"
+                localResult = "cloudStatus-green"
+                self.affectedServices = ""
+                self.alert_ImageView.image = self.alert_image_green
+                self.alert_message = "\nJamf Cloud: All systems go."
+                self.serviceCount = 0
+                self.displayAlert(currentState: localResult)
+            }
+            
+            print("operationalArray: \(operationalArray)\n")
+            print("warningArray: \(warningArray)\n")
+            print("criticalArray: \(criticalArray)\n")
+            
+            if (localResult != "cloudStatus-green") && (localResult != "cloudStatus-yellow") && (localResult != "cloudStatus-red") {
+                self.iconName = "minimizedIcon"
+            } else {
+                self.iconName =  localResult
+            }
+
+          completion(localResult)
+        })   // let task - end
+        task.resume()
+        print("")
+        //        JSON parsing - end
+     }
     
     func readSettings() -> NSMutableDictionary? {
 //        let fileManager = FileManager.default
@@ -259,19 +254,6 @@ class StatusMenuController: NSObject, URLSessionDelegate {
         } else {
             return .none
         }
-    }
-    
-    func statusMsg(theLine: String) -> String {
-        var range: Range<String.Index> = statusLine.range(of: ">")!
-        let startIndex: Int = statusLine.distance(from: statusLine.startIndex, to: range.lowerBound)
-        range = statusLine.range(of: "</a>")!
-        let endIndex: Int = statusLine.distance(from: statusLine.startIndex, to: range.lowerBound)
-        
-        let start = statusLine.index(statusLine.startIndex, offsetBy: startIndex+1)
-        let end = statusLine.index(statusLine.startIndex, offsetBy: endIndex)
-        let substrintRange = start..<end
-        
-        return statusLine.substring(with: substrintRange)
     }
 
 }
